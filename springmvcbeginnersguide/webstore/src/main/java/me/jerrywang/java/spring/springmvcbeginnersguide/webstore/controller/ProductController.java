e5012a1ce6088ea1cfa2d6ab5d6651a16099aea7
Jerry Wang <j3rry.wan9@gmail.com>
2017-09-12 19:40:58 -0700

    add webstore/products

---
+++ springmvcbeginnersguide/webstore/src/main/java/me/jerrywang/java/spring/springmvcbeginnersguide/webstore/controller/ProductController.java
@@ -0,0 +1,26 @@
+package me.jerrywang.java.spring.springmvcbeginnersguide.webstore.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.jerrywang.java.spring.springmvcbeginnersguide.webstore.domain.Product;
+
+@Controller
+public class ProductController {
+
+  @RequestMapping("/products")
+  public String list(final Model model) {
+    final Product iphone = new Product("P1234","iPhone 6s", new BigDecimal(500));
+    iphone.setDescription("Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
+    iphone.setCategory("Smartphone");
+    iphone.setManufacturer("Apple");
+    iphone.setUnitsInStock(1000);
+
+    model.addAttribute("product", iphone);
+
+    return "products";
+  }
+}
---
+++ springmvcbeginnersguide/webstore/src/main/java/me/jerrywang/java/spring/springmvcbeginnersguide/webstore/domain/Product.java
@@ -0,0 +1,28 @@
+package me.jerrywang.java.spring.springmvcbeginnersguide.webstore.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class Product implements Serializable {
+  private static final long serialVersionUID = -7596961173064726280L;
+
+  private String productId;
+  private String name;
+  private BigDecimal unitPrice;
+  private String description;
+  private String manufacturer;
+  private String category;
+  private long unitsInStock;
+  private long unitsInOrder;
+  private boolean discontinued;
+  private String condition;
+
+  public Product(final String productId, final String name, final BigDecimal unitPrice) {
+    this.productId = productId;
+    this.name = name;
+    this.unitPrice = unitPrice;
+  }
+}
