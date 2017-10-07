c9b05d2ef9ec0b549cfcfd21f7f3acb169ea1010
Jerry Wang <j3rry.wan9@gmail.com>
2017-10-06 17:03:24 -0700

    update springmvcbeginnersguide/webstore/src/main/java/me/jerrywang/java/spring/springmvcbeginnersguide/webstore/domain/Product.java

--- springmvcbeginnersguide/webstore/src/main/java/me/jerrywang/java/spring/springmvcbeginnersguide/webstore/domain/Product.java
+++ springmvcbeginnersguide/webstore/src/main/java/me/jerrywang/java/spring/springmvcbeginnersguide/webstore/domain/Product.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Product implements Serializable {
   private static final long serialVersionUID = -7596961173064726280L;
 
